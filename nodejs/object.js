d8bc19c754c32c6141d2718f229f3c6e6981e6d3
jamesjoel <james.joelj@gmail.com>
2021-10-25 16:38:25 +0530

    variables and data-types

---
+++ nodejs/object.js
@@ -0,0 +1,11 @@
+var data = {
+    name : "rohit",
+    age : 25,
+    city : "indoer",
+    gender : "male",
+    contact : 1234456,
+    email : "rohit@gmail.com",
+    merried : true
+};
+
+console.log(data.email);
