f2ee46e5f7a6a6409f06c6ae827cf02470fd3e7f
jamesjoel <james.joelj@gmail.com>
2021-10-26 16:27:30 +0530

    basic node

---
+++ nodejs/loop1.js
@@ -0,0 +1,9 @@
+var x = 5;
+for(var a = 1; a <= 10; a++ )
+{
+    console.log(x);
+    x += 5;
+}
+
+
+
---
+++ nodejs/loop2.js
@@ -0,0 +1,8 @@
+var a = 10;
+var ans=0;
+
+for(var x = a; x >= 1; x--)
+{
+    ans += x;
+}
+console.log(ans);
