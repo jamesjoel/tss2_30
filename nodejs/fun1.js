fa5f86fea8e119f68530c34b0779e0c77c1cea34
jamesjoel <james.joelj@gmail.com>
2021-10-27 16:24:36 +0530

    module and functions

---
+++ nodejs/fun1.js
@@ -0,0 +1,14 @@
+function demo()
+{
+    var a = 50;
+    var b = 30;
+    var c = a+b;
+    var d = c/3;
+    console.log(d);
+}
+console.log("hello");
+demo();
+console.log("welcome");
+demo();
+
+// demo();
---
+++ nodejs/fun2.js
@@ -0,0 +1,14 @@
+function demo(a, b)
+{
+    // var a = 50;
+    // var b = 30;
+    var c = a+b;
+    var d = c/2;
+    console.log(d);
+}
+
+demo(100, 10);
+demo(250, 30);
+demo(350, 70);
+
+// demo();
