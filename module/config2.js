cbbef6e015f34e29b2fffb43cc718034b83f1d30
jamesjoel <james.joelj@gmail.com>
2021-10-28 15:49:23 +0530

    express day 1

---
+++ module/config2.js
@@ -0,0 +1,16 @@
+module.exports = ()=>{
+    return {
+        a : "rohit",
+        b : "nilesh",
+        c : ()=>{
+            return {
+                first_name : "james",
+                last_name : "joel",
+                demo : ()=>{
+                    console.log("----------------");
+                }
+
+            }
+        }
+    }
+}
