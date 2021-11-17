6ab689fe4b9f74d9c148f5ffa5856e5807c9c84a
jamesjoel <james.joelj@gmail.com>
2021-11-17 13:57:29 +0530

    view and view all

--- exp5/controllers/TeacherController.js
+++ exp5/controllers/TeacherController.js
@@ -2,23 +2,62 @@ var express = require("express");
 var routes = express.Router();
 var mongodb = require("mongodb");
 var MongoClient = mongodb.MongoClient;
-
+var mongoUrl = "mongodb://localhost:27017";
 
 
 routes.get("/", (req, res)=>{
-    res.render("teacher");
+
+    MongoClient.connect(mongoUrl, (err, con)=>{
+        var db = con.db("tss2");
+        db.collection("teacher").find().toArray((err, result)=>{
+        //   console.log(result);
+            var pageData = { result : result };
+            res.render("teacher", pageData);
+        })
+    })
+
+
+
+
 })
+
+
+
 
 routes.get("/add", (req, res)=>{
     res.render("add_teacher");
 })
 
+
+routes.get("/view/:a", (req, res)=>{
+    // console.log(req.params.a);
+    var id = req.params.a; // 729
+
+
+    var objid = mongodb.ObjectId(id); // ObjectId(729)
+    // 645
+
+    MongoClient.connect(mongoUrl, (err, con)=>{
+        var db = con.db("tss2");
+        db.collection("teacher").find({ _id : objid }).toArray((err, result)=>{
+            console.log(result);
+            var pageData = { result : result[0] };
+            res.render("view_teacher", pageData);
+        })
+    })
+
+})
+
+
+
+
+
 routes.post("/save", (req, res)=>{
     // console.log(req.body);
-    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
+    MongoClient.connect(mongoUrl, (err, con)=>{
         var db = con.db("tss2");
         db.collection("teacher").insertOne(req.body, ()=>{
-            res.redirect("/");
+            res.redirect("/teacher");
         })
     })
 })
