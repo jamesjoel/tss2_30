974e774e1ec94fd505582e77dfe047af1e78bf6f
jamesjoel <jamesjoelj@gmail.com>
2021-12-09 14:25:03 +0530

    signup in angular

--- project1/src/app/user/pages/signup/signup.component.ts
+++ project1/src/app/user/pages/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { checkRePass, checkNum, checkLength } from '../../../helpers/custome.validation';
+import { UserService } from '../../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -11,9 +13,12 @@ export class SignupComponent implements OnInit {
 
   regForm:FormGroup;
   checkForm = false;
+  afterSubmit = false;
 
   constructor(
-    private _fb : FormBuilder
+    private _fb : FormBuilder,
+    private _user : UserService,
+    private _router : Router
   ) {
     this.regForm = this._fb.group({
       fullname : ["", Validators.required],
@@ -38,6 +43,17 @@ export class SignupComponent implements OnInit {
       this.checkForm=true;
       return;
     }
+    // console.log(this.regForm.value);
+    this._user.save(this.regForm.value).subscribe(result=>{
+      // console.log(result);
+      if(result.success == true)
+      {
+        this.afterSubmit =  true;
+        this.regForm.reset();
+        this.checkForm = false;
+        // this._router.navigate(["/login"]);
+      }
+    })
   }
 
 }
--- project_api/controllers/UserController.js
+++ project_api/controllers/UserController.js
@@ -1,8 +1,25 @@
 var express = require("express");
 var routes = express.Router();
+var database = require("../config/database");
+var mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
+var collName = "user";
+var sha1 = require("sha1");
 
-routes.get("/", (req, res)=>{
-    res.send({ success : true });
+
+routes.post("/", (req, res)=>{
+    delete req.body.re_password;
+    req.body.password = sha1(req.body.password);
+    
+    MongoClient.connect(database.dbUrl, (err, con)=>{
+        var db = con.db(database.dbName);
+        db.collection(collName).insertOne(req.body, ()=>{
+            res.send({ success : true });
+        })
+    })
+
+
+
 })
 
 
